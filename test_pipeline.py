98e7b23982f87f1521375450383799a1629ba469
majd osama saleem <5732997@gmail.com>
2026-06-27 18:41:41 +0300

    update test_pipline file

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,5 +2,4 @@ from django.test import SimpleTestCase
 
 class ActionSanityTest(SimpleTestCase):
     def test_environment_is_healthy(self):
-        """فحص بسيط للتأكد من أن بيئة العمل والمكتبات تعمل بأمان على السيرفر"""
         self.assertTrue(True)
